fafe04fcf37f8b6bccbb0205db7aafb999a86f3b
samaco634 <72288990+samaco634@users.noreply.github.com>
2023-01-09 14:36:19 +0900

    Update and rename jd_5_object_detection_opencv.py to jd_5_object_detection_haar.py

--- jd_5_object_detection_haar.py
+++ jd_5_object_detection_haar.py
@@ -6,6 +6,8 @@ from adafruit_servokit import ServoKit
 from jd_deep_lane_detect import JdDeepLaneDetect
 from jd_car_motor_l9110 import JdCarMotorL9110
 
+import jd_object_detect_haar as haar
+
 '''
 2. Creating object from classes
   1) Servo handling object from ServoKit class 
@@ -73,32 +75,12 @@ While on driving, driving is recorded.
 # real driving routine
 while cap.isOpened():
     #isStop, isImg, stopImage = objectDetectThread.getStopSign()
+    
     ret,  img = cap.read()
-
-    print(datetime.datetime.now().strftime("%H:%M:%S"))
     
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    stop_sign_scaled = stop_sign.detectMultiScale(gray, 1.3, 5)
-    
-    if len(stop_sign_scaled) == 0 :
-        isStop = False
-    else:
-        # Detect the stop sign, x,y = origin points, w = width, h = height
-        for (x, y, w, h) in stop_sign_scaled:
-            # Draw rectangle around the stop sign
-            stop_sign_rectangle = cv2.rectangle(img, (x,y),
-                                                (x+w, y+h),
-                                                (0, 255, 0), 3)
-            # Write "Stop sign" on the bottom of the rectangle
-            stop_sign_text = cv2.putText(img=stop_sign_rectangle,
-                                         text="Stop Sign",
-                                         org=(x, y+h+30),
-                                         fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                                         fontScale=1, color=(0, 0, 255),
-                                         thickness=2, lineType=cv2.LINE_4)
-            if w > stop_width or h > stop_height:
-                isStop = True
-    
+    if ret :
+      isStop, img = haar.isStopSignDetected()
+   
     cv2.imshow('object detection', img)
 
 
